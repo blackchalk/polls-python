8d5b2b8c7f7625832eddd2315ed9f2a5799a6790
Alvin Raygon <araygon@msi-ecs.com.ph>
2018-12-18 13:22:33 +0800

    customizing the admin page

--- polls/admin.py
+++ polls/admin.py
@@ -1,5 +1,36 @@
 from django.contrib import admin
 #Make the poll app modifiable in the admin
-from .models import Question
+from .models import Question, Choice
 
-admin.site.register(Question)
+#admin.site.register(Question)
+
+#Customizing admin form
+#You’ll follow this pattern – create a model admin class, then pass it as the second argument to admin.site.register() – any time you need to change the admin options for a model.
+# class QuestionAdmin(admin.ModelAdmin):
+
+    # fields = ['pub_date','question_text']
+
+#split the form up into fieldsets:
+#     fieldsets = [
+#         (None, {'fields':['question_text']}),
+#         ('Date Information', {'fields':['pub_date']}),
+#     ]
+
+# admin.site.register(Question,QuestionAdmin)
+
+# admin.site.register(Choice)
+
+# class ChoiceInline(admin.StackedInline):
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+
+admin.site.register(Question, QuestionAdmin)
